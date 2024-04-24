51a5d5e8ec4b568c15760b10207ccd553930c905
André Dorneles <andre@imperiosoft.com.br>
2024-04-24 10:58:14 -0300

    Implement logging and configuration initialization
    Added a logging system for better debugging and error tracking. Implemented a configuration initialisation to centralise settings. A new "Opportunity" struct was introduced to the schemas, and new dependencies added to Go modules.

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/AndreD23/go-api/router"
+import (
+	"github.com/AndreD23/go-api/config"
+	"github.com/AndreD23/go-api/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize Config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
+
+	// Initialize Router
 	router.BuildAndRun()
 }
