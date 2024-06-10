55e801745eaa37d0a3dcdef2ceb77e73d15a07f4
André Dorneles <andre.pereira@quikdev.com.br>
2024-06-10 18:28:39 -0300

    Enhance ShowOpportunityHandler functionality
    The enhanced ShowOpportunityHandler can now fetch a single opportunity using an id. It validates the id parameter and handles errors like 'invalid id' or 'opportunity with given id not found'. This commit aims to improve data fetching accuracy and error handling.

--- handler/showOpportunity.go
+++ handler/showOpportunity.go
@@ -1,12 +1,38 @@
 package handler
 
 import (
+	"errors"
+	"fmt"
+	"github.com/AndreD23/go-api/schemas"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"net/http"
+	"strconv"
 )
 
 func ShowOpportunityHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET opportunity",
-	})
+	id := ctx.Param("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "paramPath").Error())
+		return
+	}
+
+	opportunity := schemas.Opportunity{}
+
+	intID, err := strconv.Atoi(id)
+	if err != nil {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "paramPath").Error())
+		return
+	}
+
+	// Find Opportunity
+	err = db.First(&opportunity, intID).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opportunity with id %s not found", id))
+		return
+	}
+
+	fmt.Println(opportunity)
+
+	sendSuccess(ctx, "show-opportunity", opportunity)
 }
