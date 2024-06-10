6b6812cfc557db9c4f303afbf0be5cc9949a1242
André Dorneles <andre.pereira@quikdev.com.br>
2024-06-10 19:05:24 -0300

    Add Swagger documentation and incorporate it to router
    This commit introduces Swagger documentation for the API through `swaggo`. The document files are created in a new `docs` package and the swagger UI is integrated into the project router. Additionally, necessary dependencies are added and updated in the `go.mod` file.

--- router/routes.go
+++ router/routes.go
@@ -1,13 +1,20 @@
 package router
 
 import (
+	docs "github.com/AndreD23/go-api/docs"
 	"github.com/AndreD23/go-api/handler"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func loadRoutes(router *gin.Engine) {
 	// Initialize Handler
 	handler.InitializeHandler()
+
+	basePath := "/api/v1"
+
+	docs.SwaggerInfo.BasePath = basePath
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -15,7 +22,7 @@ func loadRoutes(router *gin.Engine) {
 		})
 	})
 
-	v1 := router.Group("/api/v1")
+	v1 := router.Group(basePath)
 	{
 		v1.GET("/opportunity", handler.ListOpportunitiesHandler)
 		v1.GET("/opportunity/:id", handler.ShowOpportunityHandler)
@@ -23,4 +30,7 @@ func loadRoutes(router *gin.Engine) {
 		v1.PUT("/opportunity/:id", handler.UpdateOpeningHandler)
 		v1.DELETE("/opportunity/:id", handler.DeleteOpeningHandler)
 	}
+
+	// Initialize Swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
